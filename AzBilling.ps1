08849224f343b7daca9971b5e097dc4dd8be1cb6
Ayan <ayanmullick@gmail.com>
2025-08-30 21:22:36 -0500

    .

--- AzBilling.ps1
+++ AzBilling.ps1
@@ -5,31 +5,36 @@ Invoke-WebRequest -Uri $inv.downloadurl -outfile ('c:\billing\' + $inv.name + '.
 
 
 
-#Create Subscription
+#region Create Subscription
 Install-Module Az.Billing -Verbose
 Install-Module Az.Subscription -AllowPrerelease -Verbose
 
 Get-AzEnrollmentAccount  #One needs to be added in the Accounts tab in the EA portal
+Get-AzBillingAccount
+Get-AzBillingProfile -BillingAccountName '<>'
 
-https://azure.microsoft.com/en-in/offers/ms-azr-0003p/
-purchase payG
+#Purchase payG : https://azure.microsoft.com/en-in/offers/ms-azr-0003p/
 
 #sign up for  an enterprise subscription.  This creates your Azure profile in the back end
 
-New-AzSubscription -OfferType MS-AZR-0017P -Name "Dev Team Subscription" -EnrollmentAccountObjectId <enrollmentAccountObjectId> -OwnerObjectId <userObjectId1>,<servicePrincipalObjectId>
+New-AzSubscription -OfferType 'MS-AZR-0017P' -Name "Dev Team Subscription" -EnrollmentAccountObjectId '<enrollmentAccountObjectId>' -OwnerObjectId '<userObjectId1>','<servicePrincipalObjectId>'
 
 
-New-AzSubscription -OfferType MS-AZR-0017P -Name AzSQL_PoC -EnrollmentAccountObjectId '<>' -OwnerObjectId '<>' -Verbose
+New-AzSubscription -OfferType 'MS-AZR-0017P' -Name 'AzSQL_PoC' -EnrollmentAccountObjectId '<>' -OwnerObjectId '<>' -Verbose
+#endregion
+
+
+
 
 #region create budget for resource group
 #Part 1 – create an action group with the email recipient who will receive the budget notifications / emails. (This section only works on an EA subscription)
 
-$email1 = New-AzActionGroupReceiver ` -EmailAddress username@doman.com -Name AppCosts01
-$ActionGroupId = (Set-AzActionGroup -ResourceGroupName resourcegroupname01 ` -Name AppCosts01 -ShortName AppCosts01 -Receiver $email1).Id
+$email1 = New-AzActionGroupReceiver -EmailAddress 'username@doman.com' -Name AppCosts01
+$ActionGroupId = (Set-AzActionGroup -ResourceGroupName resourcegroupname01  -Name AppCosts01 -ShortName AppCosts01 -Receiver $email1).Id
 
 #Part 2 – create the budget on the resource group
-new-AzConsumptionBudget -ResourceGroupName resourcegroupname01 -Amount 100 ` -Name AppCosts01 ` -Category Cost ` -TimeGrain Monthly ` -StartDate 2022-11-01 `
-    -EndDate 2030-05-31 ` -ContactEmail username@doman.com -NotificationKey Key1 ` -NotificationThreshold 10 -NotificationEnabled -ContactGroup $ActionGroupId
+New-AzConsumptionBudget -ResourceGroupName resourcegroupname01 -Amount 100  -Name AppCosts01  -Category Cost  -TimeGrain Monthly  -StartDate '2022-11-01' `
+    -EndDate '2030-05-31' -ContactEmail 'username@doman.com' -NotificationKey Key1 -NotificationThreshold 10 -NotificationEnabled -ContactGroup $ActionGroupId
 
 
 
@@ -59,11 +64,11 @@ New-AzConsumptionBudget -ResourceGroupName $RG.ResourceGroupName -Name ('MonthBu
 
 
 #region Get cost by resource group
-Get-AzConsumptionUsageDetail -ResourceGroup TemplateTest -StartDate 2023-07-01 -EndDate 2023-07-31  #Details each resource cose for each day for one month
-Get-AzConsumptionUsageDetail -ResourceGroup TemplateTest -StartDate 2023-07-01 -EndDate 2023-07-31| Measure-Object PretaxCost -Sum #Resource group total
+Get-AzConsumptionUsageDetail -ResourceGroup TemplateTest -StartDate '2023-07-01' -EndDate '2023-07-31'  #Details each resource cose for each day for one month
+Get-AzConsumptionUsageDetail -ResourceGroup TemplateTest -StartDate '2023-07-01' -EndDate '2023-07-31'| Measure-Object PretaxCost -Sum #Resource group total
 #By resource group. Worked
 Get-AzResourceGroup|select -First 50 -Property ResourceGroupName,
-@{n='Cost';e={[Math]::Round((Get-AzConsumptionUsageDetail -ResourceGroup $PSItem.ResourceGroupName -StartDate 2023-07-01 -EndDate 2023-07-31| Measure-Object PretaxCost -Sum).Sum,0)}}|
+@{n='Cost';e={[Math]::Round((Get-AzConsumptionUsageDetail -ResourceGroup $PSItem.ResourceGroupName -StartDate '2023-07-01' -EndDate '2023-07-31'| Measure-Object PretaxCost -Sum).Sum,0)}}|
         FT -AutoSize
 #endregion
 
