925d3e1180f8a47517195d8195998cf8136f056f
Ayan <ayanmullick@gmail.com>
2025-12-07 10:31:52 -0600

    .

--- Governance/AzQuota.ps1
+++ Governance/AzQuota.ps1
@@ -1,3 +1,6 @@
+$verbosePreference = "Continue"
+Register-AzResourceProvider -ProviderNamespace 'Microsoft.Quota'
+
 $subId = (Get-AzContext).Subscription.Id
 $scope = "subscriptions/$subId/providers/Microsoft.Network/locations/northcentralus"
 Get-AzQuota -Scope $scope
